eda9bbd875ee3dc0d15d3574e987e1acc64975d8
Konstantin Balandin <kostabalandin169@gmail.com>
2026-07-16 22:51:06 +0700

    create main module for connect routers

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from src.core.logging_settings import logger 
+from contextlib import asynccontextmanager
+from sqlalchemy import text
+from src.core.database.db import engine
+from src.api.endpoints import router as base_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("The application is running")
+    try:
+        async with engine.connect() as conn:
+            await conn.execute(text("SELECT 1"))
+    except Exception as e:
+        logger.error(f"Database connection error:{e}")
+    yield 
+    await engine.desponse()
+    logger.info("Ready")
+
+
+app = FastAPI(title="production-RAG-service", lifespan=lifespan)
+
+app.include_router(router=base_router)
